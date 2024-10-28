2f706f2d0d7eb98907a961d17f26fbbd887aa5d1
Sara <123300249+sarav929@users.noreply.github.com>
2024-10-28 10:08:45 +0000

    create classes for project and task with constructors

--- src/appLogic.js
+++
@@ -1,7 +0,0 @@
-const hi = () => {
-    console.log('Hi!')
-    const body = document.querySelector('body')
-    body.innerHTML = 'Hi!'
-}
-
-export default hi
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import hi from "./appLogic"
-import "../src/styles.css"
+import Project from './project'
+import Task from './task'
 
-hi()
+let newTask1 = new Task("Do this", "28/10/24", "", ["checklist1", "checklist2"])
+let newTask2 = new Task("Do that", "31/10/24", "do that by the due date", [])
+
+let newProj = new Project("New Project", "28/10/24", "This is a new project", "", [newTask1, newTask2])
+
+console.log(newProj)
